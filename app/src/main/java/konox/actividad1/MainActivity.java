67c8ecd0d4e2c17e5ae219b74edebb696bb56b3a
konox95 <konox95@gmail.com>
2016-11-08 11:55:41 +0100

    Commit inicial, parte de diseño hecha en el ejercicio1

---
+++ app/src/main/java/konox/actividad1/MainActivity.java
@@ -0,0 +1,13 @@
+package konox.actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
